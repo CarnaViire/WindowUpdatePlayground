afb6f0739541974c3911d32eabd389553a9abfbf
Natalia Kondratyeva <knatalia@microsoft.com>
2020-11-10 21:57:09 +0000

    Initial commit

---
+++ WindowUpdateClient/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowUpdateClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = args[0];
+            int port = int.Parse(args[1]);
+
+            int initialWindowSize = 65535;
+            int windowUpdateTreshold = initialWindowSize / 8;
+
+            for (int i = 0; i < 5; ++i)
+            {
+                byte[] buffer = new byte[initialWindowSize];
+                MemoryStream drainage = new MemoryStream();
+
+                using (var socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    socket.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+                    Console.WriteLine($"\n[{DateTime.Now.TimeOfDay}] Socket connected to {socket.RemoteEndPoint}: {socket.Connected}");
+
+                    byte[] windowUpdateMsg = BitConverter.GetBytes(windowUpdateTreshold);
+                    int totalRead = 0;
+                    int pendingWindowUpdate = 0;
+
+                    int bytesAvailable = 0;
+                    ManualResetEventSlim bytesAddedEvent = new ManualResetEventSlim();
+                    object syncObject = new object();
+
+                    CancellationTokenSource cts = new CancellationTokenSource();
+                    CancellationToken token = cts.Token;
+
+                    Stopwatch s = Stopwatch.StartNew();
+
+                    socket.Send(BitConverter.GetBytes(initialWindowSize)); // init
+
+                    // reading data from server
+                    var readThread = new Thread(() => 
+                        {
+                            while (true)
+                            {
+                                int bytesReceived = socket.Receive(buffer); // ugh... doing rubbish in bytes, need spanning and circling the buffer
+                                totalRead += bytesReceived;
+
+                                if (bytesReceived <= 0)
+                                {
+                                    cts.Cancel();
+                                    break;
+                                }
+
+                                lock (syncObject)
+                                {
+                                    bytesAvailable += bytesReceived;
+                                    if (bytesAvailable > buffer.Length)
+                                    {
+                                        throw new Exception("Buffer overflow");
+                                    }
+                                    bytesAddedEvent.Set();
+                                }
+                            }
+                        });
+                    readThread.Start();
+
+                    // draining data to memory stream and sending window updates
+                    while (!token.IsCancellationRequested)
+                    {
+                        if (Volatile.Read(ref bytesAvailable) == 0)
+                        {
+                            try
+                            {
+                                bytesAddedEvent.Wait(token);
+                            }
+                            catch (OperationCanceledException)
+                            {
+                                Console.WriteLine("Done");
+                                break;
+                            }
+                        }
+
+                        int writeSize = bytesAvailable;
+                        drainage.Write(buffer[0..writeSize]); // sorry byte rubbish again
+
+                        lock (syncObject)
+                        {
+                            bytesAvailable -= writeSize;
+                            bytesAddedEvent.Reset();
+                        }
+
+                        pendingWindowUpdate += writeSize;
+                        if (pendingWindowUpdate >= windowUpdateTreshold)
+                        {
+                            pendingWindowUpdate = 0;
+                            socket.Send(windowUpdateMsg);
+                        }
+                    }
+
+                    Console.WriteLine($"WINDOW_UPDATE {windowUpdateTreshold/1024} Kb: Elapsed {s.ElapsedMilliseconds} ms -- {(totalRead/s.ElapsedMilliseconds*1000.0 / 1024 / 1024)} Mb/s");
+                }
+            }
+        }
+    }
+}
---
+++ WindowUpdateServer/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowUpdateServer
+{
+    class Program
+    {
+        const int maxFrameSize = 16384;
+        const int fileSize = 25 * 1024 * 1024;
+
+        static void Main(string[] args)
+        {
+            string ip = args[0];
+            int port = int.Parse(args[1]);
+
+            Random random = new Random(fileSize);
+            byte[] file = new byte[fileSize];
+            random.NextBytes(file);
+
+            using (var server = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.Bind(new IPEndPoint(IPAddress.Parse(ip), port));
+                Console.WriteLine($"[{DateTime.Now.TimeOfDay}] Socket bound to {server.LocalEndPoint}");
+
+                server.Listen();
+
+                while (true)
+                {
+                    Console.WriteLine("\nWaiting for client to connect...");
+                    Socket accept = server.Accept();
+                    Console.WriteLine($"[{DateTime.Now.TimeOfDay}] Socket connected to {accept.RemoteEndPoint}");
+
+                    long bytesAvailable = 0;
+                    ManualResetEventSlim bytesAddedEvent = new ManualResetEventSlim();
+                    object syncObject = new object();
+
+                    CancellationTokenSource cts = new CancellationTokenSource();
+                    CancellationToken token = cts.Token;
+
+                    // reading window updates from client
+                    var readThread = new Thread(() => 
+                        {
+                            byte[] windowSizeMsg = new byte[4];
+                            while (!token.IsCancellationRequested)
+                            {
+                                accept.Receive(windowSizeMsg);
+                                int windowSize = BitConverter.ToInt32(windowSizeMsg);
+                                lock (syncObject)
+                                {
+                                    if (windowSize < 0)
+                                    {
+                                        throw new Exception("negative windowSize");
+                                    }
+                                    bytesAvailable = checked(bytesAvailable + windowSize);
+                                    bytesAddedEvent.Set();
+                                }
+                            }
+                        });
+                    readThread.Start();
+
+                    // sending the data
+                    int totalSent = 0;
+                    int currentPercent = 0;
+                    while (true)
+                    {
+                        if (Volatile.Read(ref bytesAvailable) == 0)
+                        {
+                            bytesAddedEvent.Wait();
+                        }
+                        int windowSize;
+                        lock (syncObject)
+                        {
+                            if (bytesAvailable == 0)
+                            {
+                                throw new Exception("Bytes not available");
+                            }
+                            windowSize = (int)Math.Min(bytesAvailable, maxFrameSize);
+                            bytesAvailable -= windowSize;
+                            if (bytesAvailable < 0)
+                            {
+                                throw new Exception("negative bytes");
+                            }
+                            bytesAddedEvent.Reset();
+                        }
+
+                        Span<byte> buffer = file[totalSent..Math.Min(totalSent+windowSize, file.Length)];
+
+                        int sent = accept.Send(buffer);
+                        if (sent != buffer.Length)
+                        {
+                            throw new Exception("unexpected sent bytes " + sent);
+                        }
+                        totalSent += sent;
+                        
+                        int newPercent = (int)(totalSent * 100L / fileSize);
+                        if (newPercent > currentPercent)
+                        {
+                            Console.WriteLine($"[{DateTime.Now.TimeOfDay}] {newPercent}%");
+                            currentPercent = newPercent;
+                        }
+
+                        if (totalSent == fileSize) 
+                        {
+                            Console.WriteLine($"[{DateTime.Now.TimeOfDay}] Done");
+                            break;
+                        }
+                    }
+
+                    cts.Cancel(); // cancel read thread
+
+                    accept.Disconnect(true);
+                }
+            }
+        }
+    }
+}
